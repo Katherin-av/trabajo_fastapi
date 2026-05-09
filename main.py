27492eab19ad4864971e5a296d94c488858f0fa8
Katherin-av <kathevelez1102@gmail.com>
2026-05-09 00:19:54 +0000

    Proyecto rutas.

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def inicio(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="inicio.html", context={"ano_actual": datetime.now().year},
+    )
+
+
+@app.get("/inicio", response_class=HTMLResponse)
+async def inicio(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="inicio.html", context={"ano_actual": datetime.now().year},
+    )
+
+
+@app.get("/productos", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="productos.html", context={"ano_actual": datetime.now().year},
+    )
+
+
+@app.get("/contacto", response_class=HTMLResponse)
+async def contacto(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="contacto.html", context={"ano_actual": datetime.now().year},
+    )
+
+@app.get("/cuidadopiel", response_class=HTMLResponse)
+async def contacto(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="cuidadopiel.html", context={"ano_actual": datetime.now().year},
+    )
